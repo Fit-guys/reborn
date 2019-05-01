5a7a32d17cb2bb027e869c0ac6b5d8c0d16fe0c3
maksimushka <ivasenko182@gmail.com>
2019-05-01 16:38:14 +0300

    last commit

--- src/components/game/game-map/GameMapScene.js
+++ src/components/game/game-map/GameMapScene.js
@@ -1,16 +1,20 @@
 import Phaser from 'phaser';
-import Num1 from '../../assets/number_1.png';
+import back2PNG from '../../assets/back2.png';
 
 export default class ExampleScene extends Phaser.Scene {
   preload() {
     this.cameras.main.backgroundColor.setTo(255, 255, 255);
-    this.load.image('step1', Num1);
+    this.load.image('back2', back2PNG);
   }
 
   create() {
-    const step1 = this.add.image(300, 400, 'step1');
-    step1.scaleX = 0.5;
-    step1.scaleY = 0.5;
+    const imag = this.add.image(0, 0, 'back2', back2PNG).setOrigin(0, 0).setInteractive();
+    imag.scaleX = window.innerWidth / imag.width;
+    imag.scaleY = window.innerHeight / imag.height;
+    imag.on('pointerdown', () => {
+      // eslint-disable-next-line no-console
+      console.log('pointerover');
+    });
   }
 
   static update() {
